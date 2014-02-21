06e8099460a0e7017c5db56230e09ac7c2b3b9fd
Sam Mason <sam.jbmason@gmail.com>
2014-02-21 09:47:48 +0000

    Add gulp-cache

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     imagemin = require('gulp-imagemin'),
     minify = require('gulp-minify-css'),
-    exec = require('gulp-exec');
+    exec = require('gulp-exec'),
+    cache = require('gulp-cache');
 
 gulp.task('css', function() {
   gulp.src('assets/css/main.scss')
@@ -16,8 +17,15 @@ gulp.task('jekyll', function() {
     .pipe(exec('bundle exec jekyll build -w'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('assets/css/**/*.scss', ['css']);
+gulp.task('images', function() {
+  gulp.src('assets/img/*')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('assets/img'))
 });
 
-gulp.task('default', ['css', 'jekyll', 'watch']);
+gulp.task('watch', function() {
+  gulp.watch('assets/css/**/*.scss', ['css']);
+  gulp.watch('assets/img/*', ['images']);
+});
+
+gulp.task('default', ['css', 'images', 'jekyll', 'watch']);

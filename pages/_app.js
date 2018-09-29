12bdebe61ad624fae1481bc0c2a06307e322a170
Sam Mason <sam.mason@rapha.cc>
2018-09-29 21:58:05 +0100

    Make static with dockerfile

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import Head from 'next/head'
 import SiteLayout from '../components/SiteLayout'
 import GlobalStyles from '../styles/global'
 import BorderStyles from '../styles/border'
@@ -9,6 +10,13 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <Container>
+        <Head>
+          <title>Sam Mason, Developer</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
         <GlobalStyles />
         <BorderStyles />
         <SiteLayout>

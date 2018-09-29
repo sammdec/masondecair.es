263178e1cb8d4571aee92377a940a81d4a703001
Sam Mason <sam.mason@rapha.cc>
2018-09-29 22:30:59 +0100

    Add tracking script

--- pages/_app.js
+++ pages/_app.js
@@ -11,12 +11,25 @@ export default class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>Sam Mason, Developer</title>
+          <title>Sam Mason, Developer in London</title>
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
           />
           <link rel="icon" href="/static/images/favicon.png" />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(g,s,q,r,d){r = g[r] = g[r] || function () {
+              (r.q = r.q || []).push(
+                arguments)
+            }; d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+            d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+            insertBefore(d,q)}(window,document,'script','_gs');
+          
+            _gs('GSN-564854-X');`
+            }}
+          />
         </Head>
         <GlobalStyles />
         <BorderStyles />
